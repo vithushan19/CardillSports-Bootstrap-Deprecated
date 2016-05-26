39f7e6481a10ba01abf653e05c65afa7e038be4e
Vithushan Nama <vithushan@spotify.com>
2016-05-26 03:05:47 -0400

    move files, rename images

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,9 @@ var jumboHeight = $('.jumbotron').outerHeight();
 function parallax(){
     var scrolled = $(window).scrollTop();
     $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+    $('.draft-retro').css('height', (jumboHeight-scrolled) + 'px');
     $('.power-of-veto').css('height', (jumboHeight-scrolled) + 'px');
+    $('.not-so-fast').css('height', (jumboHeight-scrolled) + 'px');
 }
 
 $(window).scroll(function(e){
